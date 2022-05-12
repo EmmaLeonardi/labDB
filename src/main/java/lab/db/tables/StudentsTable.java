177f1086f82c8001c5ee9a342984f4a0ccab1745
Emma Leonardi - emma.leonardi2@studio.unibo.it <emma.leonardi2@studio.unibo.it>
2022-05-12 12:20:06 +0200

    Implemented save

--- src/main/java/lab/db/tables/StudentsTable.java
+++ src/main/java/lab/db/tables/StudentsTable.java
@@ -65,36 +65,39 @@ public final class StudentsTable implements Table<Student, Integer> {
 	}
 
 	/**
-      * Given a ResultSet read all the students in it and collects them in a List
-      * @param resultSet a ResultSet from which the Student(s) will be extracted
-      * @return a List of all the students in the ResultSet
-      */
-     private List<Student> readStudentsFromResultSet(final ResultSet resultSet) {
-         // Create an empty list, then
-         // Inside a loop you should:
-         //      1. Call resultSet.next() to advance the pointer and check there are still rows to fetch
-         //      2. Use the getter methods to get the value of the columns
-         //      3. After retrieving all the data create a Student object
-         //      4. Put the student in the List
-         // Then return the list with all the found students
+	 * Given a ResultSet read all the students in it and collects them in a List
+	 * 
+	 * @param resultSet a ResultSet from which the Student(s) will be extracted
+	 * @return a List of all the students in the ResultSet
+	 */
+	private List<Student> readStudentsFromResultSet(final ResultSet resultSet) {
+		// Create an empty list, then
+		// Inside a loop you should:
+		// 1. Call resultSet.next() to advance the pointer and check there are still
+		// rows to fetch
+		// 2. Use the getter methods to get the value of the columns
+		// 3. After retrieving all the data create a Student object
+		// 4. Put the student in the List
+		// Then return the list with all the found students
 
-         // Helpful resources:
-         // https://docs.oracle.com/javase/7/docs/api/java/sql/ResultSet.html
-         // https://docs.oracle.com/javase/tutorial/jdbc/basics/retrieving.html
-    	 final List<Student> list=new ArrayList<>();
-    	 if(resultSet!=null) {
-    	 try {
-			while(resultSet.next()) {
-				 list.add(new Student(resultSet.getInt("id"), resultSet.getString("firstName"), resultSet.getString("lastName"), Optional.of(resultSet.getDate("birthday"))));
-				
-			 }
-		} catch (SQLException e) {
-			// Handle possibile sql exception
-			e.printStackTrace();
+		// Helpful resources:
+		// https://docs.oracle.com/javase/7/docs/api/java/sql/ResultSet.html
+		// https://docs.oracle.com/javase/tutorial/jdbc/basics/retrieving.html
+		final List<Student> list = new ArrayList<>();
+		if (resultSet != null) {
+			try {
+				while (resultSet.next()) {
+					list.add(new Student(resultSet.getInt("id"), resultSet.getString("firstName"),
+							resultSet.getString("lastName"), Optional.of(Utils.sqlDateToDate(resultSet.getDate("birthday")))));
+
+				}
+			} catch (SQLException e) {
+				// Handle possibile sql exception
+				e.printStackTrace();
+			}
 		}
-    	 }
-         return list;
-     }
+		return list;
+	}
 
 	@Override
 	public List<Student> findAll() {
@@ -107,12 +110,23 @@ public final class StudentsTable implements Table<Student, Integer> {
 
 	@Override
 	public boolean dropTable() {
-		throw new UnsupportedOperationException("TODO");
+		
 	}
 
 	@Override
 	public boolean save(final Student student) {
-		throw new UnsupportedOperationException("TODO");
+		final String query = "INSERT INTO " + TABLE_NAME + " VALUES (?,?,?,?)";
+		try (final PreparedStatement statement = this.connection.prepareStatement(query)) {
+			statement.setInt(1, student.getId());
+			statement.setString(2, student.getFirstName());
+			statement.setString(3, student.getLastName());
+			statement.setDate(4, Utils.dateToSqlDate(student.getBirthday().orElseGet(null)));
+			statement.executeQuery(query);
+			return true;
+		} catch (final SQLException e) {
+			// 3. Handle possible SQLExceptions
+			return false;
+		}
 	}
 
 	@Override
